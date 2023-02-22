352c208488d0c4ac8017893627db122d1244992f
giacomocaiazzo <giacomo.caiazzo2@gmail.com>
2023-02-22 11:23:55 +0100

    Aggiungo il codice finale

---
+++ CubeAI6.0/Debug/X-CUBE-AI/App/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../X-CUBE-AI/App/network.c \
+../X-CUBE-AI/App/network_data.c 
+
+OBJS += \
+./X-CUBE-AI/App/network.o \
+./X-CUBE-AI/App/network_data.o 
+
+C_DEPS += \
+./X-CUBE-AI/App/network.d \
+./X-CUBE-AI/App/network_data.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+X-CUBE-AI/App/%.o X-CUBE-AI/App/%.su: ../X-CUBE-AI/App/%.c X-CUBE-AI/App/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F469xx -c -I../Core/Inc -I../Middlewares/ST/AI/Inc -I../X-CUBE-AI/App -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/ST/STM32_MotionAC_Library/Inc -I../Middlewares/ST/STM32_MotionAW_Library/Inc -I../Middlewares/ST/STM32_MotionEC_Library/Inc -I../Middlewares/ST/STM32_MotionFX_Library/Inc -I../Middlewares/ST/STM32_MotionGC_Library/Inc -I../Middlewares/ST/STM32_MotionID_Library/Inc -I../Middlewares/ST/STM32_MotionMC_Library/Inc -I../Middlewares/ST/STM32_MotionPM_Library/Inc -I../Middlewares/ST/STM32_MotionPW_Library/Inc -I../Middlewares/ST/STM32_MotionTL_Library/Inc -I../Middlewares/ST/ARM/DSP/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-X-2d-CUBE-2d-AI-2f-App
+
+clean-X-2d-CUBE-2d-AI-2f-App:
+	-$(RM) ./X-CUBE-AI/App/network.d ./X-CUBE-AI/App/network.o ./X-CUBE-AI/App/network.su ./X-CUBE-AI/App/network_data.d ./X-CUBE-AI/App/network_data.o ./X-CUBE-AI/App/network_data.su
+
+.PHONY: clean-X-2d-CUBE-2d-AI-2f-App
+
